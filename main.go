69a88a636fb743c4bf611fc519da0c34d4a6494d
Michael Harris <dudemous@gmail.com>
2026-03-11 10:50:10 -0700

    add help & exit commands

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			input := scanner.Text()
-			lower := strings.ToLower(input)
-			words := strings.Fields(lower)
-			if len(words) > 0 {
-				firstWord := words[0]
-				fmt.Println("Your command was:", firstWord)
-			} else {
-				fmt.Println("No command given.")
-			}
-		}
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,11 +1,62 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, ok := getCommands()[commandName]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
